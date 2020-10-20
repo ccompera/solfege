0aac498bf083dadf9471060eebf82bcf8fa72b44
Cécile Compérat <ccompera@student.42.fr>
2020-10-20 21:54:56 +0200

    Use ocamlformat for a pretty look

--- note.ml
+++ note.ml
@@ -1,47 +1,27 @@
-type name = A | B | C | D | E | F | G
+type name =
+  | A
+  | B
+  | C
+  | D
+  | E
+  | F
+  | G
 [@@deriving show]
 
 type t = {
-	name : name;
-	tone_up : float;
-	tone_down : float;
+  name : name;
+  tone_up : float;
+  tone_down : float;
 }
 [@@deriving show]
 
-let scale = [
-	{
-		name = C;
-		tone_up = 1.0;
-		tone_down = 0.5;
-	};
-	{
-		name = D;
-		tone_up = 1.0;
-		tone_down = 1.0;
-	};
-	{
-		name = E;
-		tone_up = 0.5;
-		tone_down = 1.0;
-	};
-	{
-		name = F;
-		tone_up = 1.0;
-		tone_down = 0.5;
-	};
-	{
-		name = G;
-		tone_up = 1.0;
-		tone_down = 1.0;
-	};
-	{
-		name = A;
-		tone_up = 1.0;
-		tone_down = 1.0;
-	};
-	{
-		name = B;
-		tone_up = 0.5;
-		tone_down = 1.0;
-	}
-]
+let scale =
+  [
+    { name = C; tone_up = 1.0; tone_down = 0.5 };
+    { name = D; tone_up = 1.0; tone_down = 1.0 };
+    { name = E; tone_up = 0.5; tone_down = 1.0 };
+    { name = F; tone_up = 1.0; tone_down = 0.5 };
+    { name = G; tone_up = 1.0; tone_down = 1.0 };
+    { name = A; tone_up = 1.0; tone_down = 1.0 };
+    { name = B; tone_up = 0.5; tone_down = 1.0 };
+  ]
