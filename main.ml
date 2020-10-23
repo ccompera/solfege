db3e5b6647016f9af660c4f8c414648b5002d57e
Cécile Compérat <ccompera@student.42.fr>
2020-10-23 13:49:54 +0200

    Use random to get notes and interval

--- main.ml
+++ main.ml
@@ -77,38 +77,50 @@ let go_round_down origin interval =
   let c, i = find_down_note origin interval in
   (go_round origin c.Note.name interval [ (origin, c, i) ] find_down_note, false)
 
+let random_note () =
+  let n = List.nth Note.scale (Random.int (List.length Note.scale)) in
+  n.Note.name
+
+let random_interval () =
+  let i = List.nth Interval.intervals (Random.int (List.length Interval.intervals)) in
+  i.Interval.name
+
 let main lang =
+  Random.self_init ();
   let transl = match lang with `En -> Transl.en | `Fr -> Transl.fr in
+  let n1 = random_note () in
+  let n2 = random_note () in
+  let i = random_interval () in
   printf "Solfege\n";
   printf "==========";
   prompt ();
-  printf "%a" transl#q_find_up_note (Note.B, Interval.Third);
+  printf "%a" transl#q_find_up_note (n1, i);
   prompt ();
-  let dest, interval = find_up_note Note.B Interval.Third in
-  printf "%a" transl#a_find_up_note (Note.B, dest, interval);
+  let dest, interval = find_up_note n1 i in
+  printf "%a" transl#a_find_up_note (n1, dest, interval);
   printf "==========";
   prompt ();
-  printf "%a" transl#q_find_down_note (Note.B, Interval.Third);
+  printf "%a" transl#q_find_down_note (n1, i);
   prompt ();
-  let dest, interval = find_down_note Note.B Interval.Third in
-  printf "%a" transl#a_find_down_note (Note.B, dest, interval);
+  let dest, interval = find_down_note n1 i in
+  printf "%a" transl#a_find_down_note (n1, dest, interval);
   printf "==========";
   prompt ();
-  printf "%a" transl#q_find_intervals (Note.B, Note.C);
+  printf "%a" transl#q_find_intervals (n1, n2);
   prompt ();
-  let intervals = find_intervals Note.B Note.C in
-  printf "%a" transl#a_find_intervals (Note.B, Note.C, intervals);
+  let intervals = find_intervals n1 n2 in
+  printf "%a" transl#a_find_intervals (n1, n2, intervals);
   printf "==========";
   prompt ();
-  printf "%a" transl#q_go_round (Note.B, Interval.Third, true);
+  printf "%a" transl#q_go_round (n1, i, true);
   prompt ();
-  let lst, _ = go_round_up Note.B Interval.Third in
+  let lst, _ = go_round_up n1 i in
   transl#a_go_round (lst, true);
   printf "==========";
   prompt ();
-  printf "%a" transl#q_go_round (Note.B, Interval.Third, false);
+  printf "%a" transl#q_go_round (n1, i, false);
   prompt ();
-  let lst, _ = go_round_down Note.B Interval.Third in
+  let lst, _ = go_round_down n1 i in
   transl#a_go_round (lst, false);
   printf "==========\n"
 
