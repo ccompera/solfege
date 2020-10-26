5177fcd2f45c23f50cfc0c3dca07d7265f8bd124
Cécile Compérat <ccompera@student.42.fr>
2020-10-26 13:57:59 +0100

    Very simple web version

--- main.ml
+++ main.ml
@@ -2,11 +2,22 @@ open Format
 open Lwt.Syntax
 open Js_of_ocaml
 open Js_of_ocaml_lwt
+let fpaf = Format.asprintf
+let doc = Dom_html.document
 
 let prompt () =
   printf "%!";
-  let* _ = Lwt_js_events.click Dom_html.document##.body in
+  let* _ = Lwt_js_events.click doc##.body in
   Lwt.return ()
+
+let modify_elem id content =
+  let elem = Dom_html.getElementById id in
+  elem##.textContent := Js.some (Js.string content)
+  (* Dom.appendChild elem (doc##createTextNode (Js.string content)); *)
+
+let printjs id content =
+  modify_elem id content;
+  if id = "question" then modify_elem "answer" ""
 
 let random_note () = List.nth Note.scale (Random.int (List.length Note.scale))
 
@@ -24,10 +35,12 @@ let rec playrec transl =
   let n2 = random_note () in
   let i = random_interval () in
   let q = random_question (Question.questions transl) in
-  printf "%a" q.q (n1, n2, i, q.up);
+  let qt = fpaf "%a" q.q (n1, n2, i, q.up) in
+  printjs "question" qt;
   let* _ = prompt () in
-  printf "%a" q.a (q.func n1 n2 i, q.up);
-  printf "==========";
+  let at = fpaf "%a" q.a (q.func n1 n2 i, q.up) in
+  printjs "answer" at;
+  (* printf "=========="; *)
   let* _ = prompt () in
   playrec transl
 
